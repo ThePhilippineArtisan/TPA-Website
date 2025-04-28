7fb5778e24444100225035c4e2dc673490e0e0ea
AvoirJoseph <balut399@gmail.com>
2025-04-28 22:43:34 +0800

    third commit, finished the barebones of navbar

--- frontend/src/Components/NavbarComponent.jsx
+++ frontend/src/Components/NavbarComponent.jsx
@@ -1,15 +1,53 @@
 import TPAWhite from "/TPA-White.png";
+import TPACircleLogo from "/TPACircleLogo.png";
+
 import "../CSS/Navbar.css"
 
 const NavbarComponent = () => {
+    // Functionalities
+        
+        // States
+        //    const [loading, setLoading] = useState(false);
 
-    return(
-        <div className = "navbar-box">
-            <div className = "tpa-logo" id = "tpa-logo">
-                <img src = {TPAWhite} alt = "The Philippine Artisan Logo"/>
+        // Full window reload
+
+        const handleReload = () => {
+        //    setLoading(true);
+
+        //    setTimeout(() => {
+                window.location.reload();
+        //    }, 2000);
+        };
+
+    // What you see here are the nuts and bolts
+
+        return(
+            <div className = "navbar-box">
+                <div className = "tpa-logo" id = "tpa-logo">
+
+                    <img 
+                        src = {TPAWhite} 
+                        alt = "The Philippine Artisan Logo" 
+                        onClick = {handleReload}          
+                        // className = {`logo ${loading ? 'spinning' : ''}`} // If loading = true, use class called Spinning seen in Navbar.css
+                    />       
+
+                </div>
+
+                <div className="navbar-links">
+                    <a href = "#"> Home </a>
+                    <a href = "#"> Releases </a>
+                    <a href = "#"> About Us</a>
+
+                    <div className = "tpa-circle-logo">
+                        <img
+                            src = {TPACircleLogo}
+                            onClick = "#"
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
 }
 
 export default NavbarComponent;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+
+
 import App from './App.jsx'
 import NavbarComponent from './Components/NavbarComponent.jsx'
 
 // This is the "HTML" you see when you click [Inspect Element]
+
 document.body.style.cursor = "url('/cursor.png'), auto";
 
 createRoot(document.getElementById('root')).render(
