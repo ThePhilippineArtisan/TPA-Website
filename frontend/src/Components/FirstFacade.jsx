c75be3eeae7a51f8975e260bac2f3e7fa48a3062
AvoirJoseph <balut399@gmail.com>
2025-04-29 17:35:58 +0800

    navbar done, first facade is partially complete - needs carousel thing for the right-side images

---
+++ frontend/src/Components/FirstFacade.jsx
@@ -0,0 +1,80 @@
+import KahonKalyo from "/KahonKalyo.png";
+import TPACircleLogo from "/TPACircleLogo.png";
+
+import "../CSS/FirstFacade.css"
+
+const FirstFacade = () => {
+
+    return(
+        <div className = "Literary-Showcase-First-Facade">
+            <div className = "Dark-BG-First-Facade">
+
+                <div className = "DBFF-Left-Side">
+                    <div className = "DBFF-Headline">
+                        <h1>KALYO: KAHON — ‘24-‘25</h1>
+                    </div>
+
+                    <div className = "DBFF-Top-Paragraph">
+                        <h4>
+                        <span> Ang Kalyo ay ang opisyal na literary folio ng The Philippine Artisan - Manila. <br></br> </span>
+
+                        <span> 
+                            Ang kahon ay laberinto ng mga lihim at <br></br>    
+                            inaagiw na alaala.<br></br>
+                        </span>
+
+                        <br></br>
+                    
+                        <span> 
+                            Sa mga sulok ng kuwadrado, matatagpuan ang <br></br>
+                            katotohanan na pilit itinatago. <br></br> 
+                        </span>
+                        </h4>
+                    </div>
+
+                    <div className = "Small-Prev-Image-Showcase">
+                        <img
+                            src = {TPACircleLogo}
+                            onClick = "#"
+                        /> 
+                            <h4>
+                                <span>The 81-year old official student publication of Technological University of the Philippines Manila.</span>
+                                <br></br>                                <br></br>
+                                <span>Serving the academe and the country since 1944.</span>
+                            </h4>
+                    </div>
+
+
+
+                    <div className = "DBFF-Bottom-Paragraph">
+                        <h4>
+                            <span> 
+                                Ngunit kung tatalikuran, bawat hakbang,
+                                bawat yapak ay katumbas ng katuturan <br></br>
+                                ng mundo ang papasanin.<br></br>
+                            </span>
+
+                            <br></br>
+                        
+                            <span> 
+                                Sa mga sulok ng kuwadrado, matatagpuan ang <br></br>
+                                katotohanan na pilit itinatago. <br></br> 
+                            </span>
+                        </h4>
+                    </div>
+                </div>
+                <div className = "DBFF-Right-Side">
+                    <div className = "First-Facade-Image-Showcase">
+                            <img
+                                src = {KahonKalyo}
+
+                            />
+                        </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default FirstFacade;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import NavbarComponent from './Components/NavbarComponent.jsx'
+import FirstFacade from './Components/FirstFacade.jsx'
 
 // This is the "HTML" you see when you click [Inspect Element]
 
@@ -13,6 +14,7 @@ document.body.style.cursor = "url('/cursor.png'), auto";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavbarComponent />
+    <FirstFacade />
     <App />
   </StrictMode>,
 )
