c1fc2ca278b2b53500a551859fbad72144a37293
AvoirJoseph <balut399@gmail.com>
2026-06-10 18:04:25 +0800

    Finished AdminPageLogIn

--- frontend/src/Pages/AdminPageLogIn.jsx
+++ frontend/src/Pages/AdminPageLogIn.jsx
@@ -3,23 +3,32 @@ import "../CSS/AdminPageLogIn.css"
 const AdminPageLogIn = () => {
     return(
         <div className = "Admin-Log-In-Full-Page">
-            <div className = "Admin-Log-In-Form">
-                <div>
-                    <h1> Admin Log-In </h1>
-                </div>
-
-                <div>
+            <div className = "Admin-Log-In-Form-Container">
+                <div className = "Admin-Log-In-Form">
                     <div>
-                        <h3> Email Address: <input></input> </h3>
+                        <h1> Admin Portal </h1>
+                        <p> Sign-in using your provided credentials to explore the dashboard</p>
                     </div>
-                    <div>
-                        <h3> Password: <input></input> </h3>
+                    <div className = "Admin-Mid-Bottom-Part">
+                        <div className = "Admin-Log-In-Form-Inputs">
+                            <div>
+                                <p> EMAIL ADDRESS </p>
+                                <input
+                                    placeholder = "credentials@gmail.com"
+                                />
+                            </div>
+                            <div>
+                                <p> PASSWORD </p>
+                                <input
+                                    placeholder="••••••••" 
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div>
-                    <button> Log-In </button>
-                </div>
+                    <div className = "Admin-Log-In-Form-Button">
+                        <button> <h2>Log-In</h2></button>
+                    </div>
+                </div>    
             </div>
         </div>
     )
