fac4f3019bf9288fe521592047fcb138162c6259
AvoirJoseph <balut399@gmail.com>
2026-07-26 17:58:21 +0800

    pwa ftw

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   VitePWA({
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    devOptions: {
+      enabled: true
+    },
     manifest: {
       name: 'The Philippine Artisan',
       short_name: 'TPA',
@@ -15,16 +18,20 @@ export default defineConfig({
       theme_color: '#ffffff',
       background_color: '#ffffff',
       display: 'standalone',
-      icons: [{
-        src: 'pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }]
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     }
   })
   ],
